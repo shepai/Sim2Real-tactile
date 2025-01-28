dbbc3044592ce576104468e9b0c21b857726886c
Dexter Shepherd <dextershepherd@hotmail.co.uk>
2025-01-28 13:11:55 +0000

    Update simulation.py

--- Experiments/simulation.py
+++ Experiments/simulation.py
@@ -55,7 +55,7 @@ env = EdgeFollowEnv(
 
 # set seed for deterministic results
 env.seed(seed)
-env.action_space.np_random.seed(seed)
+#env.action_space.np_random.seed(seed)
 
 # create controllable parameters on GUI
 action_ids = []
