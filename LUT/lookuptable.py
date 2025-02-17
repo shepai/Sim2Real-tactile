e0cd7d412d6388f24cb9319eda704cca99b05936
shepai <dextershepherd@hotmail.co.uk>
2025-02-17 12:02:13 +0000

    update

--- LUT/lookuptable.py
+++ LUT/lookuptable.py
@@ -54,6 +54,8 @@ class TABLE:
         raise FileNotFoundError
     def find_nearest(self,map,dirs,time,pressure,i=0): #find the nereates image in the lookup table
         print("DEPTH",i )
+        if dirs[0]>self.maps[map].shape[1]: dirs[0]=self.maps[map].shape[1]-1
+        if dirs[1]>self.maps[map].shape[2]: dirs[1]=self.maps[map].shape[2]-1
         if np.sum(self.maps[map][dirs[0]][dirs[1]][time][pressure])==0:
             dirs[0]-=1
             image=self.find_nearest(map,dirs,time,pressure,i=i+1)
